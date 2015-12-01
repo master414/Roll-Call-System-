6a7f0e431831a753000d6579745269cc8ede8039
kuo1203 <minnie2649@gamil.com>
2015-12-01 14:44:42 +0800

    20151201 update
    doget <->dopost

--- servlet_test1/src/web/Test1.java
+++ servlet_test1/src/web/Test1.java
@@ -13,75 +13,59 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
-/**
- * Servlet implementation class Test1
- */
 @WebServlet("/Test1")
-public class Test1 extends HttpServlet {
-	
-	public String JSONFilePath = "C:\\Users\\jenny\\Downloads\\asd.json";
-	
-	
+
+public class Test1 extends HttpServlet{
+
+	public String JSONFilePath = "C:\\Program Files\\Apache Software Foundation\\Tomcat 8.0\\webapps\\Named_json\\NewMsg.json";
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 3968791874190509438L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-   /** public Test1() {
-        super();
-        // TODO Auto-generated constructor stub
-    }**/
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+	* 處理post方式提交的資料
+	*/
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		//doGet(request, response);
-
-				response.setContentType("text/html;charset=UTF-8");
-				response.setCharacterEncoding("UTF-8");
-
-				PrintWriter JSONOut = response.getWriter();
-				String JSONText = null, JSONString = null;
-
-				File f = new File(JSONFilePath);
-				StringBuilder Builder = new StringBuilder();
-
-				try {
-					BufferedReader Reader = new BufferedReader(new FileReader(f));
-					while ((JSONText = Reader.readLine()) != null) {
-						Builder.append(JSONText + "\n");
-					}
-					JSONString = Builder.toString();
-					Reader.close();
-				} catch (FileNotFoundException e) {
-					// TODO 自動產生的 catch 區塊
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO 自動產生的 catch 區塊
-					e.printStackTrace();
-				}
-
-				JSONOut.write(JSONString);
-				JSONOut.print(JSONString);
-				
-				JSONOut.flush();
-				JSONOut.close();
 		
-	}
+		response.setContentType("text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
 
+		PrintWriter JSONOut = response.getWriter();
+		String JSONText = null, JSONString = null;
+
+		File f = new File(JSONFilePath);
+		StringBuilder Builder = new StringBuilder();
+
+		try {
+			BufferedReader Reader = new BufferedReader(new FileReader(f));
+			while ((JSONText = Reader.readLine()) != null) {
+				Builder.append(JSONText + "\n");
+			}
+			JSONString = Builder.toString();
+			Reader.close();
+		} catch (FileNotFoundException e) {
+			// TODO 自動產生的 catch 區塊
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO 自動產生的 catch 區塊
+			e.printStackTrace();
+		}
+		//JSONString = new String(JSONString.getBytes("ISO-8859-1"), "UTF-8");
+		JSONOut.write(JSONString);
+		//JSONOut.print(JSONString);
+		
+		JSONOut.flush();
+		JSONOut.close();
+	}
+	
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+	* 處理get方式提交的資料
+	*/
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doPost(request, response);
-		
 	}
-
 }
